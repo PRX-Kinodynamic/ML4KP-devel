87dcb5a5924f771024b59c8659a29ebbaab8f28c
Gary <gary.granados@gmail.com>
2021-11-15 14:21:06 -0500

    CARE implementation

---
+++ src/prx/utilities/math/continuous_algebraic_riccati_equation.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "prx/utilities/general/constants.hpp"
+#include "prx/utilities/general/prx_assert.hpp"
+namespace prx 
+{
+
+/**
+** http://www.engr.iupui.edu/~skoskie/ECE684/Riccati_algorithms.pdf
+**/
+
+    class care
+    {
+        public:
+        static 
+        Eigen::MatrixXd solve(const ref_matrixXd_t& A, const ref_matrixXd_t& B, 
+            const ref_matrixXd_t& Q, const ref_matrixXd_t& R, unsigned int max_iterations = 100);
+    };
+
+}
