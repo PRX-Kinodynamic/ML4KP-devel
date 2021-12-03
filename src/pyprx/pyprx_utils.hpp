8a69c6b891cc64b9e672f4e7c9d35acf0e27a89d
Gary <gary.granados@gmail.com>
2021-12-03 16:11:39 -0500

    Adding python __str__ to iterables

--- src/pyprx/pyprx_utils.hpp
+++ src/pyprx/pyprx_utils.hpp
@@ -18,7 +18,7 @@ using namespace boost::python;
  */
 #define PRX_FUNC_WRAPPER(OBJ, PTR, FUNC) void OBJ##_##FUNC##_wrapper(prx::OBJ& o, std::shared_ptr<prx::PTR> smart_p) { o.FUNC(smart_p.get()); }
 
-#define PRX_ITERABLE_WRAPPER(CLASS, NAME) class_<CLASS>(NAME).def(vector_indexing_suite<CLASS>());
+#define PRX_ITERABLE_WRAPPER(CLASS, NAME) class_<CLASS>(NAME).def(vector_indexing_suite<CLASS>()).def("__str__", &iter_to_str<CLASS>); 
 
 
 /*
@@ -30,6 +30,22 @@ std::string to_str(const std::vector<T> &v)
    // using namespace std;
    std::ostringstream os;
    copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
+   return os.str();
+}
+
+template<typename T>
+std::string iter_to_str(T &v) 
+{
+   // using namespace std;
+   std::ostringstream os;
+   // copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
+   os << "[";
+   for (int i = 0; i < v.size(); ++i)
+   {
+        if(i != 0) os << ", ";
+        os << v[i];
+   }
+   os << "]";
    return os.str();
 }
 
