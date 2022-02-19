983cabecdae6220bd3dee6eaefac75bb6ceb3051
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2022-02-19 01:10:54 -0500

    Fixed goal check issue (doesn't use norm angle pi)

--- executables/local_goal/process_reachability_data.py
+++ executables/local_goal/process_reachability_data.py
@@ -1,21 +1,23 @@
 import numpy as np 
+from tqdm import tqdm
 
 data_dir = "/home/aravind/repos/ML4KP-devel/out/data_reach_treaded_vehicle/"
-num_trajs = 1
+num_trajs = 10
 
-dat = np.loadtxt(data_dir+"trajectory_0.txt",delimiter=",")
+for k in tqdm(range(num_trajs)):
+    dat = np.loadtxt(data_dir+"trajectory_"+str(k)+".txt",delimiter=",")
 
-tuples = []
+    tuples = []
 
-for i in range(dat.shape[0]):
-    if dat[i,-1] == 0:
-        for j in range(i+1,dat.shape[0]):
-            tuples.append(np.hstack([dat[i,:-1],dat[j,:-1],0]))
-    else:
-        for j in range(i+1,dat.shape[0]):
-            if dat[j,-1] == 1:
-                tuples.append(np.hstack([dat[i,:-1],dat[j,:-1],1]))
-            else: break
+    for i in range(dat.shape[0]):
+        if dat[i,-1] == 0:
+            for j in range(i+1,dat.shape[0]):
+                tuples.append(np.hstack([dat[i,:-1],dat[j,:-1],0]))
+        else:
+            for j in range(i+1,dat.shape[0]):
+                if dat[j,-1] == 1:
+                    tuples.append(np.hstack([dat[i,:-1],dat[j,:-1],1]))
+                else: break
 
-tuples = np.vstack(tuples)
-np.savetxt(data_dir+"processed_0.txt",tuples,delimiter=",")
+    tuples = np.vstack(tuples)
+    np.savetxt(data_dir+"processed_"+str(k)+".txt",tuples,delimiter=",")
