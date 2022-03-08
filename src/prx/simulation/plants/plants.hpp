c6a70bf496bdcc098e68e2977a42add1e22afa11
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2022-03-08 15:07:10 -0500

    Add initial versions of quadrotors

--- src/prx/simulation/plants/plants.hpp
+++ src/prx/simulation/plants/plants.hpp
@@ -10,3 +10,5 @@
 #include "prx/simulation/plants/three_dimensional_point.hpp"
 #include "prx/simulation/plants/treaded_vehicle_first_order.hpp"
 #include "prx/simulation/plants/double_integrator_2d.hpp"
+#include "prx/simulation/plants/quadrotor_1d.hpp"
+#include "prx/simulation/plants/quadrotor_2d.hpp"
