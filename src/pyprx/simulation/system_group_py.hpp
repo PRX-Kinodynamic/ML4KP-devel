4ab6b2314e2287e3b9ccd4d2441b2555f2a6193a
Edgar Granados <gary.granados@gmail.com>
2022-04-21 17:37:39 -0400

    adding propagate to system_group that takes controller and condition_check

--- src/pyprx/simulation/system_group_py.hpp
+++ src/pyprx/simulation/system_group_py.hpp
@@ -2,6 +2,10 @@
 #include <boost/python.hpp>
 #include "prx/simulation/system_group.hpp"
 
+
+void  (prx::system_group_t::*propagate_4)(prx::space_point_t, prx::controller_ptr_t, prx::condition_check_t&, prx::space_point_t) = &prx::system_group_t::propagate;
+
+
 void pyprx_simulation_system_group_py()
 {
 	register_ptr_to_python< std::shared_ptr<prx::system_group_t> >();
@@ -12,5 +16,6 @@ void pyprx_simulation_system_group_py()
 		// .def("__init__", make_constructor(&create_ptr<prx::system_group_t, prx::system_group_t>, default_call_policies() ))
 		.def("get_state_space", &prx::system_group_t::get_state_space, return_internal_reference<>())
 		.def("get_control_space", &prx::system_group_t::get_control_space, return_internal_reference<>())
+		.def("propagate", propagate_4)
 		;
 }
