5d35d75d0072dd457dc3befc629283a69efdf495
Edgar Granados <gary.granados@gmail.com>
2022-04-21 17:36:34 -0400

    Noisy_controller - New class

--- src/pyprx/simulation/controllers/controllers_py.hpp
+++ src/pyprx/simulation/controllers/controllers_py.hpp
@@ -1,8 +1,10 @@
 #include "pyprx/simulation/controllers/lqr_py.hpp"
 #include "pyprx/simulation/controllers/ackermann_FO_ctrl_py.hpp"
+#include "pyprx/simulation/controllers/noisy_controller_py.hpp"
 
 void pyprx_simulation_controllers()
 {
 	pyprx_simulation_controllers_lqr();
 	pyprx_simulation_controllers_ackermann_FO_ctrl();
+	pyprx_simulation_controllers_noisy();
 }
