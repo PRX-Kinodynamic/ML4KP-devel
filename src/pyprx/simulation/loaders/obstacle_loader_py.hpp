3dc4ab3839490c4cd6ecdb17949a6ef511a95386
Gary Granados <eoseguda@outrider.ai>
2022-07-28 12:19:20 -0600

    Adding ref to python bindings constructor

--- src/pyprx/simulation/loaders/obstacle_loader_py.hpp
+++ src/pyprx/simulation/loaders/obstacle_loader_py.hpp
@@ -5,7 +5,7 @@ void pyprx_simulation_loaders_obstacle_loader()
 {
 	
     class_<prx::obstacle_loader_t, std::shared_ptr<prx::obstacle_loader_t>>("obstacle_loader", no_init)
-		.def("__init__", make_constructor(&init_as_ptr<prx::obstacle_loader_t, const std::string >, default_call_policies()))
+		.def("__init__", make_constructor(&init_as_ptr<prx::obstacle_loader_t, const std::string& >, default_call_policies()))
 		.def("get_names", &prx::obstacle_loader_t::get_names)
 		.def("get_obstacles", &prx::obstacle_loader_t::get_obstacles)
     ;
