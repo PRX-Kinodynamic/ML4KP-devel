e15489f1230f17ceb78a557d8590d0505853ff43
Edgar Granados <gary.granados@gmail.com>
2022-09-27 13:56:21 -0400

    Removing unnecessary includes + minor fixes

--- src/pyprx/planning/world_model_py.hpp
+++ src/pyprx/planning/world_model_py.hpp
@@ -1,27 +1,11 @@
-#include <iostream>
-#include <boost/python.hpp>
-#include <boost/python/stl_iterator.hpp>
 #include "prx/planning/world_model.hpp"
 #include "prx/simulation/system_group.hpp"
+#include <boost/python.hpp>
+#include <boost/python/stl_iterator.hpp>
+#include <iostream>
 
 using namespace boost::python;
-
-// template<class T>
-// py::list std_vector_to_py_list(const std::vector<T>& v)
-// {
-//     py::object get_iter = py::iterator<std::vector<T> >();
-//     py::object iter = get_iter(v);
-//     py::list l(iter);
-//     return l;
-// }
-
-// template< typename T >
-// inline
-// std::vector< T > to_std_vector( const py::object& iterable )
-// {
-//     return std::vector< T >( py::stl_input_iterator< T >( iterable ),
-//                              py::stl_input_iterator< T >( ) );
-// }
+// clang-format off
 
 void pyprx_planning_world_model_py()
 {
@@ -35,12 +19,10 @@ void pyprx_planning_world_model_py()
     	.def_readwrite("system_group",  &std::pair<std::shared_ptr<prx::system_group_t>, std::shared_ptr<prx::collision_group_t>>::first)
     	.def_readwrite("collision_group", &std::pair<std::shared_ptr<prx::system_group_t>, std::shared_ptr<prx::collision_group_t>>::second)
     	;
-		// void create_context(const std::string& context_name,const std::vector<std::string>& system_names,const std::vector<std::string>& obstacle_names);
-		// std::pair<,std::shared_ptr<collision_group_t>> get_context(const std::string& context_name);
-			
-   	class_<prx::world_model_t<>>("world_model", init<std::vector<prx::system_ptr_t>, std::vector<std::shared_ptr<prx::movable_object_t>>>())
-   	    .def("create_context", &prx::world_model_t<>::create_context)
-   	    .def("get_context",    &prx::world_model_t<>::get_context)
+
+   	class_<prx::world_model_t>("world_model", init<std::vector<prx::system_ptr_t>, std::vector<std::shared_ptr<prx::movable_object_t>>>())
+   	    .def("create_context", &prx::world_model_t::create_context)
+   	    .def("get_context",    &prx::world_model_t::get_context)
    	    ;
    	
 }
