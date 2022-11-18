fa3c8527c816cb0de90b96c9df7f6968cdf95d0f
Gary <gary.granados@gmail.com>
2022-11-18 11:15:46 -0500

    changing location of condition_check from planning to simulation/general (+ indentation)

--- src/prx/simulation/system_group.hpp
+++ src/prx/simulation/system_group.hpp
@@ -2,63 +2,62 @@
 
 #include "prx/utilities/defs.hpp"
 #include "prx/simulation/system.hpp"
-// #include "prx/simulation/simulator.hpp"
 #include "prx/simulation/controller.hpp"
-#include "prx/planning/condition_check.hpp"
 #include "prx/simulation/playback/plan.hpp"
 #include "prx/simulation/playback/trajectory.hpp"
 #include "prx/simulation/collision_checking/collision_checker.hpp"
+#include "prx/simulation/general/condition_check.hpp"
 namespace prx
 {
-	class simulator_t;
-	class system_group_manager_t;
+class simulator_t;
+class system_group_manager_t;
 
-	class system_group_t
-	{
-	public:
-		// system_group_t(const std::vector<system_ptr_t>& sys_group);
-		system_group_t(const std::vector<system_ptr_t>& sys_group, plant_type p_type = plant_type::ANALYTICAL);
-		~system_group_t();
+class system_group_t
+{
+public:
+  // system_group_t(const std::vector<system_ptr_t>& sys_group);
+  system_group_t(const std::vector<system_ptr_t>& sys_group, plant_type p_type = plant_type::ANALYTICAL);
+  ~system_group_t();
 
-		void propagate(space_point_t start_state, const std::shared_ptr<controller_t>& ctrl, double duration, plan_t& plan, trajectory_t& traj);
+  void propagate(space_point_t start_state, const std::shared_ptr<controller_t>& ctrl, double duration, plan_t& plan,
+                 trajectory_t& traj);
 
-		void propagate(space_point_t start_state, const plan_t& plan, space_point_t result);
-		
-		void propagate(space_point_t start_state, const plan_t& plan, trajectory_t& traj);
-		
-		void propagate(int steps, space_point_t control = nullptr, trajectory_t* traj = nullptr);
+  void propagate(space_point_t start_state, const plan_t& plan, space_point_t result);
 
-		void propagate(space_point_t start_state, controller_ptr_t ctrl, condition_check_t& cond_check, space_point_t result);
+  void propagate(space_point_t start_state, const plan_t& plan, trajectory_t& traj);
 
-		void propagate(space_point_t start_state, controller_ptr_t ctrl, condition_check_t& cond_check, trajectory_t& result);
+  void propagate(int steps, space_point_t control = nullptr, trajectory_t* traj = nullptr);
 
-		void compute_stopping_maneuver(space_point_t start_state, std::vector<double>&, std::vector<double>&);
-		
-		inline space_t* get_state_space()
-		{
-			return state_space;
-		}
+  void propagate(space_point_t start_state, controller_ptr_t ctrl, condition_check_t& cond_check, space_point_t result);
 
-		inline space_t* get_control_space()
-		{
-			return control_space;
-		}
+  void propagate(space_point_t start_state, controller_ptr_t ctrl, condition_check_t& cond_check, trajectory_t& result);
 
-		inline space_t* get_parameter_space()
-		{
-			return parameter_space;
-		}
+  void compute_stopping_maneuver(space_point_t start_state, std::vector<double>&, std::vector<double>&);
 
-		void propagate_once(propagate_step step, space_point_t control = nullptr);
+  inline space_t* get_state_space()
+  {
+    return state_space;
+  }
 
-	protected:
+  inline space_t* get_control_space()
+  {
+    return control_space;
+  }
 
-		std::vector<system_ptr_t> group;
-		space_t* state_space;
-		space_t* control_space;
-		space_t* parameter_space;
-		simulator_t* sim;
+  inline space_t* get_parameter_space()
+  {
+    return parameter_space;
+  }
 
-		friend system_group_manager_t;
-	};
-}
+  void propagate_once(propagate_step step, space_point_t control = nullptr);
+
+protected:
+  std::vector<system_ptr_t> group;
+  space_t* state_space;
+  space_t* control_space;
+  space_t* parameter_space;
+  simulator_t* sim;
+
+  friend system_group_manager_t;
+};
+}  // namespace prx
