bd92255805efb76a05053edba4d0248ab3f55792
Edgar Granados <gary.granados@gmail.com>
2022-12-22 00:46:40 -0500

    fixing compilation bug for pyprx on linux

--- src/pyprx/planning/noisy_world_model_py.hpp
+++ src/pyprx/planning/noisy_world_model_py.hpp
@@ -13,7 +13,7 @@ void bind_noisy_world_model(const std::string& name)
          boost::noncopyable>(name.c_str(), no_init)
       .def("__init__",
            make_constructor(&init_as_ptr<prx::noisy_world_model_t<T>, const std::vector<prx::system_ptr_t>&,
-                                         const std::vector<std::shared_ptr<prx::movable_object_t>>, Types...>,
+                                         const std::vector<std::shared_ptr<prx::movable_object_t>>&, Types...>,
                             default_call_policies()));
 }
 
