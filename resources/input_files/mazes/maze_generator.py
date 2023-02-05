9a4c4355788a3b2d5fd8ceab4a6263bf1f957bfd
NoahRCarver <noahrcarver1@gmail.com>
2023-02-04 20:39:02 -0500

    maze matrix generator

--- resources/input_files/mazes/maze_generator.py
+++ resources/input_files/mazes/maze_generator.py
@@ -5,6 +5,9 @@ import os
 cells_x = 20
 cells_y = 20
 cell_size = 2
+wall = '0'
+cell = ' '
+unvisited = '.'
 
 random.random()
 
@@ -17,7 +20,7 @@ def random_number_uniform(min=-max_bound,max=max_bound):
 	# return 1
 
 def create_maze(fname,pt1=[0,0],pt2=[0,0]):
-	f = open(fname,'w')
+	f = open(fname,wall)
 	f.write("environment:\n")
 	f.write("  type: obstacle\n")
 	f.write("  geometries:\n")
@@ -47,7 +50,7 @@ def create_maze(fname,pt1=[0,0],pt2=[0,0]):
 	f.close()
 
 def create_3D_maze(fname,pt1=[0,0],pt2=[0,0]):
-	f = open(fname,'w')
+	f = open(fname,wall)
 	f.write("environment:\n")
 	f.write("  type: obstacle\n")
 	f.write("  geometries:\n")
@@ -77,6 +80,225 @@ def create_3D_maze(fname,pt1=[0,0],pt2=[0,0]):
 				f.write("        orientation: [0,0,0,1]\n")
 				break
 	f.close()
+# Find number of surrounding cells
+def surroundingCells(maze, rand_wall):
+	s_cells = 0
+	if (maze[rand_wall[0]-1][rand_wall[1]] == cell):
+		s_cells += 1
+	if (maze[rand_wall[0]+1][rand_wall[1]] == cell):
+		s_cells += 1
+	if (maze[rand_wall[0]][rand_wall[1]-1] == cell):
+		s_cells +=1
+	if (maze[rand_wall[0]][rand_wall[1]+1] == cell):
+		s_cells += 1
+
+	return s_cells
+	
+
+def generate_2D_mazemap(height,width):
+	global wall
+	global cell
+	global unvisited
+	## Main code
+	# Init variables
+	
+	maze = [[unvisited for i in range(height)] for j in range(width)]
+
+	# Randomize starting point and set it a cell
+	starting_height = int(random.random()*height)
+	starting_width = int(random.random()*width)
+	if (starting_height == 0):
+		starting_height += 1
+	if (starting_height == height-1):
+		starting_height -= 1
+	if (starting_width == 0):
+		starting_width += 1
+	if (starting_width == width-1):
+		starting_width -= 1
+
+	# Mark it as cell and add surrounding walls to the list
+	maze[starting_height][starting_width] = cell
+	walls = []
+	walls.append([starting_height - 1, starting_width])
+	walls.append([starting_height, starting_width - 1])
+	walls.append([starting_height, starting_width + 1])
+	walls.append([starting_height + 1, starting_width])
+
+	# Denote walls in maze
+	maze[starting_height -1 ][starting_width] = wall
+	maze[starting_height][starting_width - 1] = wall
+	maze[starting_height][starting_width + 1] = wall
+	maze[starting_height + 1][starting_width] = wall
+
+	while (walls):
+		# Pick a random wall
+		rand_wall = walls[int(random.random()*len(walls))-1]
+
+		# Check if it is a left wall
+		if (rand_wall[1] != 0):
+			if (maze[rand_wall[0]][rand_wall[1]-1] == unvisited and maze[rand_wall[0]][rand_wall[1]+1] == cell):
+				# Find the number of surrounding cells
+				s_cells = surroundingCells(maze, rand_wall)
+
+				if (s_cells < 2):
+					# Denote the new path
+					maze[rand_wall[0]][rand_wall[1]] = cell
+
+					# Mark the new walls
+					# Upper cell
+					if (rand_wall[0] != 0):
+						if (maze[rand_wall[0]-1][rand_wall[1]] != cell):
+							maze[rand_wall[0]-1][rand_wall[1]] = wall
+						if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+							walls.append([rand_wall[0]-1, rand_wall[1]])
+
+
+					# Bottom cell
+					if (rand_wall[0] != height-1):
+						if (maze[rand_wall[0]+1][rand_wall[1]] != cell):
+							maze[rand_wall[0]+1][rand_wall[1]] = wall
+						if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+							walls.append([rand_wall[0]+1, rand_wall[1]])
+
+					# Leftmost cell
+					if (rand_wall[1] != 0):	
+						if (maze[rand_wall[0]][rand_wall[1]-1] != cell):
+							maze[rand_wall[0]][rand_wall[1]-1] = wall
+						if ([rand_wall[0], rand_wall[1]-1] not in walls):
+							walls.append([rand_wall[0], rand_wall[1]-1])
+				
+
+				# Delete wall
+				for i in walls:
+					if (i[0] == rand_wall[0] and i[1] == rand_wall[1]):
+						walls.remove(i)
+
+				continue
+
+		# Check if it is an upper wall
+		if (rand_wall[0] != 0):
+			if (maze[rand_wall[0]-1][rand_wall[1]] == unvisited and maze[rand_wall[0]+1][rand_wall[1]] == cell):
+
+				s_cells = surroundingCells(maze, rand_wall)
+				if (s_cells < 2):
+					# Denote the new path
+					maze[rand_wall[0]][rand_wall[1]] = cell
+
+					# Mark the new walls
+					# Upper cell
+					if (rand_wall[0] != 0):
+						if (maze[rand_wall[0]-1][rand_wall[1]] != cell):
+							maze[rand_wall[0]-1][rand_wall[1]] = wall
+						if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+							walls.append([rand_wall[0]-1, rand_wall[1]])
+
+					# Leftmost cell
+					if (rand_wall[1] != 0):
+						if (maze[rand_wall[0]][rand_wall[1]-1] != cell):
+							maze[rand_wall[0]][rand_wall[1]-1] = wall
+						if ([rand_wall[0], rand_wall[1]-1] not in walls):
+							walls.append([rand_wall[0], rand_wall[1]-1])
+
+					# Rightmost cell
+					if (rand_wall[1] != width-1):
+						if (maze[rand_wall[0]][rand_wall[1]+1] != cell):
+							maze[rand_wall[0]][rand_wall[1]+1] = wall
+						if ([rand_wall[0], rand_wall[1]+1] not in walls):
+							walls.append([rand_wall[0], rand_wall[1]+1])
+
+				# Delete wall
+				for i in walls:
+					if (i[0] == rand_wall[0] and i[1] == rand_wall[1]):
+						walls.remove(i)
+
+				continue
+
+		# Check the bottom wall
+		if (rand_wall[0] != height-1):
+			if (maze[rand_wall[0]+1][rand_wall[1]] == unvisited and maze[rand_wall[0]-1][rand_wall[1]] == cell):
+
+				s_cells = surroundingCells(maze, rand_wall)
+				if (s_cells < 2):
+					# Denote the new path
+					maze[rand_wall[0]][rand_wall[1]] = cell
+
+					# Mark the new walls
+					if (rand_wall[0] != height-1):
+						if (maze[rand_wall[0]+1][rand_wall[1]] != cell):
+							maze[rand_wall[0]+1][rand_wall[1]] = wall
+						if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+							walls.append([rand_wall[0]+1, rand_wall[1]])
+					if (rand_wall[1] != 0):
+						if (maze[rand_wall[0]][rand_wall[1]-1] != cell):
+							maze[rand_wall[0]][rand_wall[1]-1] = wall
+						if ([rand_wall[0], rand_wall[1]-1] not in walls):
+							walls.append([rand_wall[0], rand_wall[1]-1])
+					if (rand_wall[1] != width-1):
+						if (maze[rand_wall[0]][rand_wall[1]+1] != cell):
+							maze[rand_wall[0]][rand_wall[1]+1] = wall
+						if ([rand_wall[0], rand_wall[1]+1] not in walls):
+							walls.append([rand_wall[0], rand_wall[1]+1])
+
+				# Delete wall
+				for i in walls:
+					if (i[0] == rand_wall[0] and i[1] == rand_wall[1]):
+						walls.remove(i)
+				continue
+
+		# Check the right wall
+		if (rand_wall[1] != width-1):
+			if (maze[rand_wall[0]][rand_wall[1]+1] == unvisited and maze[rand_wall[0]][rand_wall[1]-1] == cell):
+
+				s_cells = surroundingCells(maze, rand_wall)
+				if (s_cells < 2):
+					# Denote the new path
+					maze[rand_wall[0]][rand_wall[1]] = cell
+
+					# Mark the new walls
+					if (rand_wall[1] != width-1):
+						if (maze[rand_wall[0]][rand_wall[1]+1] != cell):
+							maze[rand_wall[0]][rand_wall[1]+1] = wall
+						if ([rand_wall[0], rand_wall[1]+1] not in walls):
+							walls.append([rand_wall[0], rand_wall[1]+1])
+					if (rand_wall[0] != height-1):
+						if (maze[rand_wall[0]+1][rand_wall[1]] != cell):
+							maze[rand_wall[0]+1][rand_wall[1]] = wall
+						if ([rand_wall[0]+1, rand_wall[1]] not in walls):
+							walls.append([rand_wall[0]+1, rand_wall[1]])
+					if (rand_wall[0] != 0):	
+						if (maze[rand_wall[0]-1][rand_wall[1]] != cell):
+							maze[rand_wall[0]-1][rand_wall[1]] = wall
+						if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+							walls.append([rand_wall[0]-1, rand_wall[1]])
+
+				# Delete wall
+				for i in walls:
+					if (i[0] == rand_wall[0] and i[1] == rand_wall[1]):
+						walls.remove(i)
+
+				continue
+
+		# Delete the wall from the list anyway
+		for i in walls:
+			if (i[0] == rand_wall[0] and i[1] == rand_wall[1]):
+				walls.remove(i)
+		
+
+
+	# Mark the remaining unvisited cells as walls
+	for i in range(0, height):
+		for j in range(0, width):
+			if (maze[i][j] == unvisited):
+				maze[i][j] = wall
+
+	# Print final maze
+	return maze
+
+def print_mazemap(map):
+	for i in map:
+		for j in i:
+			print(j,end="")
+		print()
 
 def create_boundaries(f):
 	f.write("    -\n")
@@ -161,6 +383,7 @@ def create_boundaries(f):
 	'''
 
 if __name__ == "__main__":
-	script_dir = os.path.dirname(__file__)
-	fname = os.path.join(script_dir,"maze.yaml")
-	create_maze(fname,[-9.5,-9.5],[9.5,9.5])
+	print_mazemap(generate_2D_mazemap(cells_y,cells_x))
+	#script_dir = os.path.dirname(__file__)
+	#fname = os.path.join(script_dir,"maze.yaml")
+	#create_maze(fname,[-9.5,-9.5],[9.5,9.5])
