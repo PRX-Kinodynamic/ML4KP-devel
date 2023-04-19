36680a95f875871144155ef93e2782e5ca3757e4
Gary <gary.granados@gmail.com>
2023-04-19 15:23:45 -0400

    car-like stuff

---
+++ src/prx/simulation/plants/car_like.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "prx/simulation/plant.hpp"
+
+namespace prx
+{
+/**
+ * @brief <b> A second-order car-like system. </b>
+ *
+ * @author Aravind Sivaramakrishnan, Edgar Granados
+ *
+ */
+class car_like_t : public plant_t
+{
+public:
+  car_like_t(const std::string& path);
+
+  virtual ~car_like_t();
+
+  virtual void propagate(const double simulation_step) override final;
+
+  virtual void update_configuration() override;
+
+  virtual void compute_derivative() override final;
+
+protected:
+  double x, y, theta, phi, v, dx, dy, dtheta, dphi, dv;
+  double L = 0.6;
+
+private:
+  std::vector<double> lower_bound = { -11, -11, -3.15, -PRX_PI / 3, -0.2 };
+  std::vector<double> upper_bound = { 11, 11, 3.15, PRX_PI / 3, 0.7 };
+  friend system_factory_t;
+};
+}  // namespace prx
+PRX_REGISTER_SYSTEM(car_like_t, car_like)
