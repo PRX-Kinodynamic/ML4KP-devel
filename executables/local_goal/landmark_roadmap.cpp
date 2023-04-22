d1a670d456bc64585327faff52e833e831e4e087
Noah Carver <noahrcarver1@gmail.com>
2023-04-22 13:22:09 -0400

    directional reachability roadmaps done

--- executables/local_goal/landmark_roadmap.cpp
+++ executables/local_goal/landmark_roadmap.cpp
@@ -48,8 +48,8 @@ int main(int argc, char* argv[])
         if (argc <= 1)
         {
             // prx_throw("This executable needs a parameter file!");
-            // params_file = "local_goal/car_like.yaml";
-            params_file = "local_goal/annotate_treaded.yaml";
+            params_file = "local_goal/car_like.yaml";
+            //params_file = "local_goal/annotate_treaded.yaml";
         }
         else 
         {
