edd086b63944eb49c3d06852ab2ed5c3f1c8f721
Edgar Granados <gary.granados@gmail.com>
2023-08-08 15:23:03 -0700

    Adding test to evaluate speed of copy_point vs copy

--- tests/utilities/spaces/test_spaces.cpp
+++ tests/utilities/spaces/test_spaces.cpp
@@ -1,5 +1,8 @@
 #define BOOST_AUTO_TEST_MAIN spaces_test
+
+#include <chrono>
 #include <string>
+
 #include <boost/test/unit_test.hpp>
 #include "prx/utilities/spaces/space.hpp"
 
@@ -196,4 +199,37 @@ BOOST_AUTO_TEST_CASE(testing_copy_from_initializer_list)
   space.copy_from({ 10, 20, 3 });
   space.copy_to(pt_sp2);
   BOOST_CHECK(space.equal_points(pt_sp1, pt_sp2));
+}
+
+BOOST_AUTO_TEST_CASE(comparing_speed_of_copy_point_vs_copy)
+{
+  mock::space3d_t test;
+  prx::space_t& space = test.space;
+
+  prx::space_point_t pt_from = space.make_point();
+  prx::space_point_t pt_to = space.make_point();
+
+  const std::size_t total_copies{100'000};
+
+  auto start = std::chrono::steady_clock::now();
+  for(std::size_t i = 0; i < total_copies; ++i;)
+  {
+    space.sample(pt_sp1);
+    pt_sp1.copy_point(pt_to, pt_from);
+  }
+  auto end = std::chrono::steady_clock::now();
+  const double elapsed_copy_point{(end - start).count()};
+
+  start = std::chrono::steady_clock::now();
+  for(std::size_t i = 0; i < total_copies; ++i;)
+  {
+    space.sample(pt_sp1);
+    pt_sp1.copy(pt_to, pt_from);
+  }
+  end = std::chrono::steady_clock::now();
+  const double elapsed_copy{(end - start).count()};
+
+  std::cout << "Results:\n"
+  std::cout << "Copy_point: " << elapsed_copy_point << "s\n";
+  std::cout << "Copy: " << elapsed_copy << "s\n";
 }
