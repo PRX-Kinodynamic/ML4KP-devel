6e06df7be1e718d2310a8a526716987bdce027d6
Gary <gary.granados@gmail.com>
2023-09-15 11:59:34 -0400

    Addressing PR comments Moving to header-only

--- src/prx/simulation/controllers/lqr.hpp
+++ src/prx/simulation/controllers/lqr.hpp
@@ -23,34 +23,34 @@ public:
   using VectorX = Eigen::Vector<double, Xdim>;
   using VectorU = Eigen::Vector<double, Udim>;
 
+  template <typename MatA, typename MatB, typename MatQ, typename MatR, typename MatK>
+  lqr_t(const MatA a, const MatB b, const MatQ q, const MatR r, const MatK k) : _A(a), _B(b), _Q(q), _R(r), _K(k)
+  {
+  }
+
+  template <Eigen::Index InputDim = Xdim, std::enable_if_t<(InputDim == Eigen::Dynamic), bool> = true>
   lqr_t(const std::size_t& xdim, const std::size_t& udim)
-    : _A(MatrixA::Zero(xdim, xdim))
-    , _B(MatrixB::Zero(xdim, udim))
-    , _Q(MatrixQ::Zero(xdim, xdim))
-    , _R(MatrixR::Zero(udim, udim))
-    , _K(MatrixK::Zero(udim, xdim))
+    : lqr_t(MatrixA::Identity(xdim, xdim), MatrixB::Identity(xdim, udim), MatrixQ::Identity(xdim, xdim),
+            MatrixR::Identity(udim, udim), MatrixK::Zero(udim, xdim))
   {
   }
 
   template <Eigen::Index InputDim = Xdim, std::enable_if_t<(InputDim != Eigen::Dynamic), bool> = true>
-  lqr_t() : lqr_t(Xdim, Udim)
+  lqr_t()
+    : lqr_t(MatrixA::Identity(Xdim, Xdim), MatrixB::Identity(Xdim, Udim), MatrixQ::Identity(Xdim, Xdim),
+            MatrixR::Identity(Udim, Udim), MatrixK::Zero(Udim, Xdim))
   {
   }
 
-  template <typename MatA, typename MatB, typename MatQ, typename MatR, typename Xref>
-  lqr_t(const MatA a, const MatB b, const MatQ q, const MatR r, const Xref x_ref)
-    : _A(a), _B(b), _Q(q), _R(r), _K(MatrixK::Zero())
+  template <typename MatA, typename MatB, typename MatQ, typename MatR>
+  lqr_t(const MatA a, const MatB b, const MatQ q, const MatR r) : lqr_t(a, b, q, r, MatrixK::Zero())
   {
     compute_K();
   }
 
-  template <typename MatA, typename MatB, typename MatQ, typename MatR>
-  lqr_t(const MatA a, const MatB b, const MatQ q, const MatR r) : lqr_t(a, b, q, r, VectorX::Zero())
-  {
-  }
-
   template <typename MatK>
-  lqr_t(const MatK k) : _A(MatrixA::Zero()), _B(MatrixB::Zero()), _Q(MatrixQ::Zero()), _R(MatrixR::Zero()), _K(k)
+  lqr_t(const MatK k)
+    : _A(MatrixA::Identity()), _B(MatrixB::Identity()), _Q(MatrixQ::Zero()), _R(MatrixR::Zero()), _K(k)
   {
   }
 
@@ -111,15 +111,24 @@ public:
     return care::solve(_A, _B, _Q, _R);
   }
 
+  // Check {Q,R} >= 0 && R invertible
+  // For big matrices, this check might be *slow*
   void compute_K()
   {
-    _K = _R.inverse() * (_B.transpose() * S());
+    const Eigen::LDLT<MatrixQ> q_ldlt{ _Q };
+    prx_assert(q_ldlt.isPositive(), "Matrix Q must be positive semi-definitve");
+    const Eigen::LDLT<MatrixR> r_ldlt{ _R };
+    prx_assert(r_ldlt.isPositive(), "Matrix R must be positive");
+    const Eigen::FullPivLU<MatrixR> r_lu{ _R };
+    prx_assert(r_lu.isInvertible(), "Matrix R must be invertible");
+
+    _K = r_lu.inverse() * (_B.transpose() * S());
   }
 
   // Computes the control u = -K * X;
   inline VectorU operator()(const VectorX& x) const
   {
-    return -_K * x;
+    return this->operator()(x, VectorX::Zero(x.size()));
   }
 
   // Computes the control u = -K * (X-X_ref);
