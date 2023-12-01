1cb8f71fb8d52c91ee715f1c1f0175dbc230cd1a
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2023-12-01 00:55:57 -0500

    Adding functions to reduce last control in  a plan

--- src/prx/simulation/playback/plan.hpp
+++ src/prx/simulation/playback/plan.hpp
@@ -180,6 +180,12 @@ public:
   void extend_last_control(double time);
 
   /**
+   * @brief Reduce the duration of the last control in the plan by a specified time.
+   * @param time Time to reduce the duration of the last control by (in seconds).
+   */
+  void reduce_last_control(double time);
+
+  /**
    * @brief Removes the first control from the plan.
    */
   void pop_front();
