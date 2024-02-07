81991e77fb21991f5928f8f266b5d22410aa6887
Gary <gary.granados@gmail.com>
2024-02-07 14:29:21 -0500

    position velocity factor

---
+++ src/prx/factor_graphs/factors/position_velocity_factor.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <array>
+#include <numeric>
+#include <gtsam/config.h>
+#include <gtsam/base/Testable.h>
+#include <gtsam/nonlinear/Expression.h>
+#include <gtsam/nonlinear/NonlinearFactor.h>
+
+namespace prx
+{
+namespace fg
+{
+
+class position_velocity_factor_t
+  : public gtsam::NoiseModelFactor3<Eigen::Vector<double, 3>, Eigen::Vector<double, 3>, Eigen::Vector<double, 3>>
+{
+  using Base = gtsam::NoiseModelFactor3<Eigen::Vector<double, 3>, Eigen::Vector<double, 3>, Eigen::Vector<double, 3>>;
+
+public:
+  using Position = Eigen::Vector3d;
+  using Velocity = Eigen::Vector3d;
+  using Jacobian = Eigen::MatrixXd;
+  using Error = gtsam::Vector;
+
+  position_velocity_factor_t(const gtsam::Key& xi, const gtsam::Key& xj, const gtsam::Key& vi, const double dt,
+                             const gtsam::noiseModel::Base::shared_ptr& cost_model)
+    : Base(cost_model, xi, xj, vi), _dt(dt)
+  {
+  }
+
+  Error evaluateError(const Position& xi, const Position& xj, const Velocity& vj,
+                      boost::optional<Jacobian&> H1 = boost::none, boost::optional<Jacobian&> H2 = boost::none,
+                      boost::optional<Jacobian&> H3 = boost::none) const override
+  {
+    const Position xjp{ xi + vj * _dt };
+
+    if (H1)
+    {
+      *H1 = Eigen::Matrix3d::Identity();
+    }
+    if (H2)
+    {
+      *H2 = -Eigen::Matrix3d::Identity();
+    }
+    if (H3)
+    {
+      *H3 = _dt * Eigen::Matrix3d::Identity();
+    }
+
+    return xjp - xj;
+  }
+
+private:
+  const double _dt;
+};
+
+}  // namespace fg
+}  // namespace prx
