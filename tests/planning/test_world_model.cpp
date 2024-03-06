fac6d4e1ed8df9771f8ce5d51903277d6e0a1be0
Edgar <gary.granados@gmail.com>
2024-03-06 12:26:47 -0500

    Implementation of functions to add obstacles after world model has been constructed.

--- tests/planning/test_world_model.cpp
+++ tests/planning/test_world_model.cpp
@@ -89,3 +89,39 @@ BOOST_AUTO_TEST_CASE(world_model_3_systems_test)
   BOOST_CHECK(context.second != nullptr);
   BOOST_CHECK(world_model.get_all_context_names()[0] == "test_context");
 }
+
+BOOST_AUTO_TEST_CASE(world_model_adding_obstacles_dynamically)
+{
+  const std::string context_name{ "test_context" };
+  const std::vector<std::string> system_names{ { "2D_Point" } };
+  std::vector<prx::system_ptr_t> plants{ { prx::system_factory_t::create_system(system_names[0], system_names[0]) } };
+
+  const Eigen::Vector2d position{ 0, 0 };
+  prx::transform_t pose{ prx::transform_t::Identity() };
+  pose.translation().head(3) = Eigen::Vector3d(1, 1, 0);
+
+  plants[0]->get_state_space()->copy_from(position);
+  const double obstacle_dim{ 2 };
+  const std::string box_name{ "obstacle_0" };
+  // World model is created without obstacles
+  // std::shared_ptr<prx::box_t> box{ std::make_shared<prx::box_t>("obstacle_0", obstacle_dim, obstacle_dim,
+  // obstacle_dim,
+  // pose)
+  // };
+
+  prx::world_model_t world_model(plants, {});
+  world_model.create_context(context_name, { system_names }, {});
+
+  std::shared_ptr<prx::collision_group_t> collision_group{ world_model.collision_group(context_name) };
+
+  BOOST_CHECK(collision_group != nullptr);
+
+  PRX_DEBUG_ITERABLE("Distances:", collision_group->get_distances().distances);
+  BOOST_CHECK(not collision_group->in_collision());  // No obstacles => no collisions
+  // BOOST_CHECK(not collision_group->in_collision());  // No obstacles => no collisions
+
+  // Check that this still works if adding box at the beginning
+  world_model.emplace_obstacle<prx::box_t>(context_name, "obstacle_0", obstacle_dim, obstacle_dim, obstacle_dim, pose);
+
+  BOOST_CHECK(collision_group->in_collision());  // The new obstacle is now in collision
+}
