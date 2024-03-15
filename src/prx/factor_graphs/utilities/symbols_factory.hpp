7176208abda459c9c1ed41f2f39e0ab6c76a6318
Gary <gary.granados@gmail.com>
2024-03-15 11:36:45 -0400

    Cmake changes to include gtsam Symbol factory

---
+++ src/prx/factor_graphs/utilities/symbols_factory.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <memory>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+#include <gtsam/inference/Key.h>
+
+#include "prx/utilities/general/constants.hpp"
+#include "prx/utilities/general/hash.hpp"
+namespace prx
+{
+namespace fg
+{
+
+using symbol_t = gtsam::Key;
+
+class symbol_factory_t
+{
+public:
+  template <typename... Ts>
+  static gtsam::Key create_hashed_symbol(std::string name, Ts... args)
+  {
+    std::size_t hash{ 0 };
+
+    const std::tuple<std::string, Ts...> ts(name, args...);
+    const std::string str{ create_hash<0>(hash, ts) };
+    const gtsam::Key key{ gtsam::Key(hash) };
+
+    symbols_map[key] = str;
+    return key;
+  }
+
+  static void symbols_to_file(std::string filename = prx::out_path + "factor_graphs_symbols.txt")
+  {
+    std::ofstream ofs;
+    ofs.open(filename.c_str(), std::ofstream::trunc);
+
+    for (auto key_str : symbols_map)
+    {
+      ofs << key_str.second << " " << key_str.first << " " << gtsam::DefaultKeyFormatter(key_str.first) << "\n";
+    }
+
+    ofs.close();
+  }
+
+  static std::string formatter(const gtsam::Key key)
+  {
+    return symbols_map[key];
+  }
+
+  template <std::size_t I, typename... Tp, std::enable_if_t<(I == sizeof...(Tp) - 1), bool> = true>
+  inline static std::string create_hash(std::size_t& hash, const std::tuple<Tp...>& t)
+  {
+    std::stringstream str;
+    str << std::get<I>(t);
+    return str.str();
+  }
+  template <std::size_t I, typename... Tp, std::enable_if_t<(I < sizeof...(Tp) - 1), bool> = true>
+  inline static std::string create_hash(std::size_t& hash, const std::tuple<Tp...>& t)
+  {
+    std::stringstream str;
+    prx::hash_combine(hash, std::get<I>(t));
+    str << std::get<I>(t);
+    str << create_hash<I + 1>(hash, t);
+
+    hash = std::hash<std::string>()(str.str());
+    return str.str();
+  }
+
+private:
+  static inline std::unordered_map<gtsam::Key, std::string> symbols_map;
+
+  symbol_factory_t(){};
+};
+
+}  // namespace fg
+}  // namespace prx
---
+++ src/prx/utilities/general/hash.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <algorithm>
+
+namespace prx
+{
+template <typename T>
+inline void hash_combine(std::size_t& seed, const T& val)
+{
+  seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template <typename T, std::size_t n_i, std::enable_if_t<(n_i == 0), bool> = true>
+inline void range_hash_combine(std::size_t& seed, const T& range)
+{
+  hash_combine(seed, range[n_i]);
+}
+
+template <typename T, std::size_t n_i, std::enable_if_t<(n_i != 0), bool> = true>
+inline void range_hash_combine(std::size_t& seed, const T& range)
+{
+  hash_combine(seed, range[n_i]);
+  range_hash_combine<T, n_i - 1>(seed, range);
+}
+
+template <typename T, std::size_t dimension>
+struct range_hash_combine_t
+{
+  const std::size_t operator()(const T& t_to_hash) const noexcept
+  {
+    std::size_t seed = 0;
+    range_hash_combine<T, dimension - 1>(seed, t_to_hash);
+    return seed;
+  }
+};
+}  // namespace prx
