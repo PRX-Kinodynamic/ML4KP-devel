2797be4d569919ec4adf5dd16057b270f8bea5d7
Gary <gary.granados@gmail.com>
2024-03-15 11:36:45 -0400

    Cmake changes to include gtsam Symbol factory

--- src/prx/utilities/general/constants.hpp
+++ src/prx/utilities/general/constants.hpp
@@ -26,7 +26,7 @@ extern char separating_value;
 namespace color
 {
 
-constexpr std::string_view normal{ "\033[0m" };
+constexpr std::string_view normal = "\033[0m";
 constexpr std::string_view red{ "\033[31m" };
 constexpr std::string_view green{ "\033[32m" };
 constexpr std::string_view yellow{ "\033[33m" };
